66c09506063bbcf8929fb880f0259727a21e5d43
LackupL58 <beekoop158+gh@gmail.com>
2025-08-19 21:56:12 -0600

    local: butt flip

--- visible_equipment_uwu/scripts/mods/visible_equipment_uwu/visible_equipment_uwu_localization.lua
+++ visible_equipment_uwu/scripts/mods/visible_equipment_uwu/visible_equipment_uwu_localization.lua
@@ -5,7 +5,11 @@ return {
 	mod_description = {
 		en = "Visible equipment positioning for unnecessarily horny people",
 	},
+	-- The local localizations are in the base mod so I don't think it'll work like this
 	placement_butt = {
 		en = "Prison Pocket",
+	},
+	placement_butt_flip = {
+		en = "Prison Pocket (Flip)",
 	},
 }
