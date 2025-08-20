367fb5571e9cfb20a0831d854ff5de6738732613
LackupL58 <beekoop158+gh@gmail.com>
2025-08-19 22:23:25 -0600

    feat: error message for missing base mod

--- visible_equipment_uwu/scripts/mods/visible_equipment_uwu/visible_equipment_uwu.lua
+++ visible_equipment_uwu/scripts/mods/visible_equipment_uwu/visible_equipment_uwu.lua
@@ -28,13 +28,18 @@ mod.visible_equipment_plugin = {
 -- ####################
 -- MY OFFSETS
 -- ####################
+-- ----------
+-- Looping through every weapon for offsets
+--      Using a generic, default value
+--      Will overwrite below if needed
+-- ----------
 for weapon_id, _ in pairs(WeaponTemplates) do
 	mod.visible_equipment_plugin.offsets[weapon_id] = {
         butt = {
             right = {
                 node = "j_hips",
                 position = vector3_box(0.005, -0.05, -0.069),
-                rotation = vector3_box(-10, 0, 90),
+                rotation = vector3_box(-5, 0, 90),
             },
         },
         butt_flip = {
@@ -46,9 +51,13 @@ for weapon_id, _ in pairs(WeaponTemplates) do
         },
     }
 end
+
+-- ----------
+-- Setting base node for placement and camera preview
+--      Only done once for each slot I add
+-- ----------
 mod.visible_equipment_plugin.placements["butt"] = "hip_back"
 mod.visible_equipment_plugin.placements["butt_flip"] = "hip_back"
-
 mod.visible_equipment_plugin.placement_camera["butt"] = {
     position = vector3_box(-1.2683889865875244, 2.639409065246582, 1.6318360567092896),
     rotation = 3.5,
@@ -58,6 +67,25 @@ mod.visible_equipment_plugin.placement_camera["butt_flip"] = {
     rotation = 3.5,
 }
 
+-- ----------
+-- Manual Overrides
+-- ----------
+mod.visible_equipment_plugin.offsets.powersword_p1_m2["butt_flip"] = {
+    right = {
+        node = "j_hips",
+        position = vector3_box(0.007, 0.0, -0.0),
+        rotation = vector3_box(140, 0, 0),
+    },
+}
+
+-- ####################
+-- HOOKS
+-- ####################
 function mod.on_all_mods_loaded()
     mod:info("v"..mod.version.." loaded uwu nya :3")
+
+    local visible_equipment = get_mod("visible_equipment")
+    if not visible_equipment then
+        mod:error("Visible Equipment mod required! The standalone mod, not Extended Weapon Customization")
+    end
 end
