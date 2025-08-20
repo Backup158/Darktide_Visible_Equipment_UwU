d190367144b488666194069f7bcbe9dddf2a883f
LackupL58 <beekoop158+gh@gmail.com>
2025-08-19 21:43:23 -0600

    feat: butthole position for sapper shovel

--- visible_equipment_uwu/scripts/mods/visible_equipment_uwu/visible_equipment_uwu.lua
+++ visible_equipment_uwu/scripts/mods/visible_equipment_uwu/visible_equipment_uwu.lua
@@ -5,20 +5,20 @@ local vector3_box = Vector3Box
 mod.visible_equipment_plugin = {
     offsets = {
         combataxe_p3_m1 = {
-            head = {
+            butt = {
                 right = {
-                    node = "j_head",
-                    position = vector3_box(0, 0, 0),
-                    rotation = vector3_box(0, 0, 90),
+                    node = "j_hips",
+                    position = vector3_box(0.0, -0.05, -0.05),
+                    rotation = vector3_box(-10, 0, 90),
                 },
             },
         },
     },
     placements = {
-        head = "head",
+        butt = "hip_back",
     },
     placement_camera = {
-        head = {
+        butt = {
             position = vector3_box(-1.2683889865875244, 2.639409065246582, 1.6318360567092896),
             rotation = 1.5,
         },
