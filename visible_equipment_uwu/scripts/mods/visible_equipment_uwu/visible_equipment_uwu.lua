cdb794eacaecea537df54fede0d116887e0624a5
LackupL58 <beekoop158+gh@gmail.com>
2025-08-19 21:55:25 -0600

    feat: head of spade as tail

--- visible_equipment_uwu/scripts/mods/visible_equipment_uwu/visible_equipment_uwu.lua
+++ visible_equipment_uwu/scripts/mods/visible_equipment_uwu/visible_equipment_uwu.lua
@@ -8,19 +8,33 @@ mod.visible_equipment_plugin = {
             butt = {
                 right = {
                     node = "j_hips",
-                    position = vector3_box(0.0, -0.05, -0.05),
+                    position = vector3_box(0.005, -0.05, -0.069),
                     rotation = vector3_box(-10, 0, 90),
+                },
+            },
+            butt_flip = {
+                right = {
+                    node = "j_hips",
+                    position = vector3_box(0.007, -0.05, 0.069),
+                    rotation = vector3_box(170, 0, 90),
                 },
             },
         },
     },
     placements = {
         butt = "hip_back",
+        butt_flip = "hip_back",
     },
     placement_camera = {
+        -- copy of hip_back
         butt = {
             position = vector3_box(-1.2683889865875244, 2.639409065246582, 1.6318360567092896),
-            rotation = 1.5,
+            rotation = 3.5,
+        },
+        -- copy of hip_back
+        butt_flip = {
+            position = vector3_box(-1.2683889865875244, 2.639409065246582, 1.6318360567092896),
+            rotation = 3.5,
         },
     }
 }
