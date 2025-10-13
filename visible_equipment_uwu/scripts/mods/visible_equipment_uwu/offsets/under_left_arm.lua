9e7d21e6eae0c89ea33921ea9c6173aaa3989631
LackupL58 <beekoop158+gh@gmail.com>
2025-10-12 20:57:17 -0600

    fix: format for placement values

--- visible_equipment_uwu/scripts/mods/visible_equipment_uwu/offsets/under_left_arm.lua
+++ visible_equipment_uwu/scripts/mods/visible_equipment_uwu/offsets/under_left_arm.lua
@@ -12,13 +12,17 @@ return {
     offsets = {
         right = {
             node = "j_hips",
-            position = vector3_box(-0.1, 0.1, 0.3),
-            rotation = vector3_box(10, -40, 180),
+            position = vector3_box(-0.16, 0.05, 0.29),
+            rotation = vector3_box(15, -10, 180),
         },
     },
-    placements = "under_left_arm",
+    placements = {
+        under_left_arm = "under_left_arm",
+    },
     placement_camera = {
-        position = vector3_box(-1.258, 2.639, 1.632),
-        rotation = 0.5,
+        under_left_arm = {
+            position = vector3_box(-1.258, 2.639, 1.632),
+            rotation = 1.5,
+        }
     },
 }
