10efeed23f150d00f460f195431645d1c2218c12
LackupL58 <beekoop158+gh@gmail.com>
2025-10-12 22:11:07 -0600

    fix: default to hip front if not given

--- visible_equipment_uwu/scripts/mods/visible_equipment_uwu/visible_equipment_uwu.lua
+++ visible_equipment_uwu/scripts/mods/visible_equipment_uwu/visible_equipment_uwu.lua
@@ -135,11 +135,11 @@ local function add_whole_offset_from_file_direct(offset_name, table_of_weapons_t
 
     -- Add placement info
     if not values_from_file.placement_camera then
-        visible_equipment_plugin.placements[offset_name] = values_from_file.placements
+        visible_equipment_plugin.placements[offset_name] = values_from_file.placements or "hip_front"
     else
         visible_equipment_plugin.placements[offset_name] = offset_name
     end
-    
+
     visible_equipment_plugin.placement_camera[offset_name] = values_from_file.placement_camera
 end
 
