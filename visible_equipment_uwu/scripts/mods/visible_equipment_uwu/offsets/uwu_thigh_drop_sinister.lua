66fd4b07cf2181ccb1397d209912d4a348278409
Backup158 <beekoop158@gmail.com>
2026-04-27 23:24:15 -0500

    feat: thigh drop holsters

---
+++ visible_equipment_uwu/scripts/mods/visible_equipment_uwu/offsets/uwu_thigh_drop_sinister.lua
@@ -0,0 +1,31 @@
+local mod = get_mod("visible_equipment_uwu")
+
+local vector3_box = Vector3Box
+
+mod:add_global_localize_strings({
+    loc_ve_placement_uwu_thigh_drop_sinister = {
+        en = "Drop Leg Holster (Sinister)",
+    }
+})
+
+return {
+    offsets = {
+        right = {
+            node = "j_leftupleg",
+            position = vector3_box(0.045, 0.035, -0.115),
+            rotation = vector3_box(-84, -4, -96),
+            center_mass = vector3_box(-.02, .1, -.07),
+        },
+        left = {
+            node = "j_rightupleg",
+            position = vector3_box(-0.031, -0.067, 0.134),
+            rotation = vector3_box(90, -4, 93),
+            center_mass = vector3_box(-.02, .1, -.07),
+        },
+    },
+    placements = "uwu_thigh_drop_sinister",
+    placement_camera = {
+        position = vector3_box(-1.258, 2.639, 1.632),
+        rotation = 0.5,
+    },
+}
--- visible_equipment_uwu/scripts/mods/visible_equipment_uwu/visible_equipment_uwu.lua
+++ visible_equipment_uwu/scripts/mods/visible_equipment_uwu/visible_equipment_uwu.lua
@@ -273,6 +273,8 @@ add_whole_offset_from_file_direct("uwu_chest_pistol", pistol_ids)
 create_sinister_offset("uwu_chest_pistol")
 add_whole_offset_from_file_direct("uwu_under_left_arm", pistol_ids)
 add_whole_offset_from_file_direct("uwu_under_right_arm", pistol_ids) -- because under_left_arm_sinister is so cursed
+add_whole_offset_from_file_direct("uwu_thigh_drop", pistol_ids)
+add_whole_offset_from_file_direct("uwu_thigh_drop_sinister", pistol_ids)
 
 local knife_ids = {"combatknife_p1_m1", "combatknife_p1_m2"}
 add_whole_offset_from_file_direct("uwu_right_ankle", knife_ids)
