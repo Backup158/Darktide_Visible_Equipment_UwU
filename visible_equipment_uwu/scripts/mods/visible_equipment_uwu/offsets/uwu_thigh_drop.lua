0e101663a8e39416f3b39571abf0ad39cab35890
Backup158 <beekoop158@gmail.com>
2026-04-27 23:42:40 -0500

    style: renamed drop holsters to thigh

--- visible_equipment_uwu/scripts/mods/visible_equipment_uwu/offsets/uwu_thigh_drop.lua
+++ visible_equipment_uwu/scripts/mods/visible_equipment_uwu/offsets/uwu_thigh_drop.lua
@@ -4,7 +4,7 @@ local vector3_box = Vector3Box
 
 mod:add_global_localize_strings({
     loc_ve_placement_uwu_thigh_drop = {
-        en = "Drop Leg Holster",
+        en = "Thigh Drop Holster",
     }
 })
 
